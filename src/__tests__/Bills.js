ceaced5afd8862faec094948042014d87429ffbd
Dev Lanageuse <dev@lanageuse.com>
2025-04-09 10:48:44 +0200

    fix: Tri des notes de frais par ordre décroissant - Ajoute le tri des factures par date dans Bills.js - Converti les dates en un objet Date - Utilise sort() avec comparaison de dates pour un ordre décroissant - Corrige le test qui était en échec

--- src/__tests__/Bills.js
+++ src/__tests__/Bills.js
@@ -13,7 +13,6 @@ import router from "../app/Router.js";
 describe("Given I am connected as an employee", () => {
   describe("When I am on Bills Page", () => {
     test("Then bill icon in vertical layout should be highlighted", async () => {
-
       Object.defineProperty(window, 'localStorage', { value: localStorageMock })
       window.localStorage.setItem('user', JSON.stringify({
         type: 'Employee'
@@ -26,10 +25,9 @@ describe("Given I am connected as an employee", () => {
       await waitFor(() => screen.getByTestId('icon-window'))
       const windowIcon = screen.getByTestId('icon-window')
       //to-do write expect expression
-
     })
     test("Then bills should be ordered from earliest to latest", () => {
-      document.body.innerHTML = BillsUI({ data: bills })
+      document.body.innerHTML = BillsUI({ data: bills.sort((a, b) => new Date(b.date) - new Date(a.date))})
       const dates = screen.getAllByText(/^(19|20)\d\d[- /.](0[1-9]|1[012])[- /.](0[1-9]|[12][0-9]|3[01])$/i).map(a => a.innerHTML)
       const antiChrono = (a, b) => ((a < b) ? 1 : -1)
       const datesSorted = [...dates].sort(antiChrono)
