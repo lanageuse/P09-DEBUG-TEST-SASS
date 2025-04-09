4527f4621bc0c7d964707bbbb81b6441aa6d126e
Dev Lanageuse <dev@lanageuse.com>
2025-04-09 11:08:18 +0200

    fix : Correction pour limiter l'upload de fichier JPEG, JPG, PNG
    - Ajout d'un tableau des types de fichiers autorisés
    - Ajout d'une condition pour vérifier si le type de fichier est présent dans le tableu
    - Affichage d'un message d'erreur dans le cas contraire

--- src/containers/NewBill.js
+++ src/containers/NewBill.js
@@ -18,10 +18,23 @@ export default class NewBill {
   handleChangeFile = e => {
     e.preventDefault()
     const file = this.document.querySelector(`input[data-testid="file"]`).files[0]
+    const allowedFileTypes = ["image/png", "image/jpeg", "image/jpg"];
+    const fileAllowed = allowedFileTypes.includes(file.type)
     const filePath = e.target.value.split(/\\/g)
-    const fileName = filePath[filePath.length-1]
+    const errorMessage = document.querySelector("#file-error");
+
+    if (!fileAllowed) {
+      e.target.value = "";
+      if (errorMessage) {
+        errorMessage.textContent = "Format de fichier invalide. Seuls les fichiers JPG, JPEG ou PNG sont autorisés.";
+      }
+      return;
+    }
+
+    const fileName = filePath[filePath.length - 1]
     const formData = new FormData()
     const email = JSON.parse(localStorage.getItem("user")).email
+    
     formData.append('file', file)
     formData.append('email', email)
 
@@ -33,12 +46,14 @@ export default class NewBill {
           noContentType: true
         }
       })
-      .then(({fileUrl, key}) => {
+      .then(({ fileUrl, key }) => {
         console.log(fileUrl)
         this.billId = key
         this.fileUrl = fileUrl
         this.fileName = fileName
+        errorMessage.textContent = ''
       }).catch(error => console.error(error))
+
   }
   handleSubmit = e => {
     e.preventDefault()
@@ -47,9 +62,9 @@ export default class NewBill {
     const bill = {
       email,
       type: e.target.querySelector(`select[data-testid="expense-type"]`).value,
-      name:  e.target.querySelector(`input[data-testid="expense-name"]`).value,
+      name: e.target.querySelector(`input[data-testid="expense-name"]`).value,
       amount: parseInt(e.target.querySelector(`input[data-testid="amount"]`).value),
-      date:  e.target.querySelector(`input[data-testid="datepicker"]`).value,
+      date: e.target.querySelector(`input[data-testid="datepicker"]`).value,
       vat: e.target.querySelector(`input[data-testid="vat"]`).value,
       pct: parseInt(e.target.querySelector(`input[data-testid="pct"]`).value) || 20,
       commentary: e.target.querySelector(`textarea[data-testid="commentary"]`).value,
@@ -65,12 +80,12 @@ export default class NewBill {
   updateBill = (bill) => {
     if (this.store) {
       this.store
-      .bills()
-      .update({data: JSON.stringify(bill), selector: this.billId})
-      .then(() => {
-        this.onNavigate(ROUTES_PATH['Bills'])
-      })
-      .catch(error => console.error(error))
+        .bills()
+        .update({ data: JSON.stringify(bill), selector: this.billId })
+        .then(() => {
+          this.onNavigate(ROUTES_PATH['Bills'])
+        })
+        .catch(error => console.error(error))
     }
   }
 }
