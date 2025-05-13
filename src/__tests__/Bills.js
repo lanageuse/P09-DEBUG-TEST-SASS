f8e5e91215c1ccc6d91539888007e87353523f27
Dev Lanageuse <dev@lanageuse.com>
2025-05-13 09:46:41 +0200

    Fix (test/Bill) : Restauration du test original pour le tri des notes de frais

--- src/__tests__/Bills.js
+++ src/__tests__/Bills.js
@@ -34,7 +34,7 @@ describe("Given I am connected as an employee", () => {
 
     // Vérifie le tri par date (du plus récent au plus ancien)
     test("Then bills should be ordered from earliest to latest", () => {
-      document.body.innerHTML = BillsUI({ data: bills.sort((a, b) => new Date(b.date) - new Date(a.date)) })
+      document.body.innerHTML = BillsUI({ data: bills })
       const dates = screen.getAllByText(/^(19|20)\d\d[- /.](0[1-9]|1[012])[- /.](0[1-9]|[12][0-9]|3[01])$/i).map(a => a.innerHTML)
       const antiChrono = (a, b) => ((a < b) ? 1 : -1)
       const datesSorted = [...dates].sort(antiChrono)
